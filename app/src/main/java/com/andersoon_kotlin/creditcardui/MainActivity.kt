378ddc89f1389bb8e39327d10e6af80258cde642
And <anderson.almeida.21012@hotmail.com>
2023-11-10 11:37:08 -0300

    Commit feito o binding e a implementação

--- app/src/main/java/com/andersoon_kotlin/creditcardui/MainActivity.kt
+++ app/src/main/java/com/andersoon_kotlin/creditcardui/MainActivity.kt
@@ -2,10 +2,33 @@ package com.andersoon_kotlin.creditcardui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.andersoon_kotlin.creditcardui.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var current = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editCreditCardNumber.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 }
