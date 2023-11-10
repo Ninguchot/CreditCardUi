69b2a2f5598536ee7b274e2b80eee027c504f95e
And <anderson.almeida.21012@hotmail.com>
2023-11-10 14:07:50 -0300

    Commit binding.editMonthAndYear

--- app/src/main/java/com/andersoon_kotlin/creditcardui/MainActivity.kt
+++ app/src/main/java/com/andersoon_kotlin/creditcardui/MainActivity.kt
@@ -42,6 +42,22 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+        binding.editMonthAndYear.addTextChangedListener(object  : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
     }
 
     companion object{
